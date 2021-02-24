c4b20e95d91d7ca43b78893c989632a89eb996cd
Apeirogon <som5@mcmaster.ca>
2021-02-24 17:03:48 -0500

    Update real_world_viz.R

--- real_world_viz.R
+++ real_world_viz.R
@@ -1,18 +1,24 @@
 library(ggplot2)
 library(ggthemes)
+library(tidyverse)
 source('ggplot_params.R')
+
 
 data = read.csv('data/Canada.csv')
 data$date = as.Date(data$date)
 
-plot = ggplot(data) + geom_line(data=data, aes(x=date, y=new_cases_per_million, color='Canada'))
+data_US = read.csv('data/United States.csv')
+data_US$date = as.Date(data_US$date)
 
-data = read.csv('data/United States.csv')
-data$date = as.Date(data$date)
+data_all = inner_join(data, data_US, by='date')
 
-plot = plot + geom_line(data=data,aes(x=date, y=new_cases_per_million, color='United States'))
+data_all = data_all %>% 
+  select(c('date','new_cases_per_million.x', 'new_cases_per_million.y')) %>%
+  rename(cdn_incidence =new_cases_per_million.x, us_incidence = new_cases_per_million.y) %>% 
+  pivot_longer(cols=!date) %>%
+  mutate(name=name %>% factor() %>% fct_relevel('us_incidence', 'cdn_incidence'))
 
-
+plot = ggplot(data_all, aes(x=date, y=value,color=name)) + geom_line()
 
 plot = plot + labs(title='New cases per million in various countries')
 plot = plot + scale_color_colorblind()
