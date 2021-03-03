c99da40094d2f5327540ae36a515a99fe27e3c2e
Apeirogon <som5@mcmaster.ca>
2021-03-03 15:57:29 -0500

    push

--- ggplot_params.R
+++ ggplot_params.R
@@ -1,5 +1,50 @@
-#! /usr/bin/Rscript
 library('ggplot2')
 library('ggthemes')
 
 theme_set(theme_bw(base_size=20))
+
+# https://stackoverflow.com/questions/57153428/r-plot-color-combinations-that-are-colorblind-accessible
+palette  <- c("#000000", "#E69F00", "#56B4E9", "#009E73", 
+                       "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# Hardcoding this palette because I don't ever intend on using a different one in this project.
+transparent_palette <- function(transparency){
+  # transparency: vector of numeric between 0-1
+  new_palette = c()
+  for(i in c(1:length(palette))){
+   if (i <= length(transparency)){
+      alpha = transparency[i]
+   }
+    
+   else{
+      alpha = 1
+   }
+    
+   alpha_hex_equiv = toupper(toString(as.hexmode(round(alpha*255))))
+   new_palette = append(new_palette, paste(palette[i],alpha_hex_equiv, sep=''))
+  }
+  
+  return (new_palette)
+}
+
+create_plot <- function(df, select_cols, col_labels, transparencies, plot_labels){
+  # df: dataframe to create plot from, in wide format. X must be a column.
+  # select_cols: the columns to be selected, excluding X. These should be in order, top to bottom, for the legend.
+  # col_labels: names of each column for ggplot legend
+  # plot_labels: From the labs function from ggplot.
+  
+  plotting_df = df %>%
+    select(c('X', select_cols)) %>%
+    pivot_longer(cols=!X) %>%
+    mutate(name=name %>% factor() %>% fct_relevel(select_cols))
+
+  plot = ggplot(plotting_df, aes(x=X, y=value, color=name)) +
+    geom_line(lwd=1) +
+    scale_color_manual(values=transparent_palette(c(1, 0.75, 0.75))) +
+    guides(colour = guide_legend(override.aes = list(alpha = 1))) +
+    plot_labels +
+    scale_fill_discrete(labels = col_labels)
+  return (plot)
+}
+
+
--- real_world_viz.R
+++ real_world_viz.R
@@ -3,24 +3,21 @@ library(ggthemes)
 library(tidyverse)
 source('ggplot_params.R')
 
+data <- read.csv('data/Canada.csv') %>% mutate(date=as.Date(date))
+data_US <- read.csv('data/United States.csv') %>% mutate(date=as.Date(date))
 
-data = read.csv('data/Canada.csv')
-data$date = as.Date(data$date)
+data_all <- inner_join(data, data_US, by='date')
 
-data_US = read.csv('data/United States.csv')
-data_US$date = as.Date(data_US$date)
-
-data_all = inner_join(data, data_US, by='date')
-
-data_all = data_all %>% 
+# While this block isn't actually necessary, I thought it was good practice for figuring out how to reorder ggplot legends.
+data_all <- data_all %>% 
   select(c('date','new_cases_per_million.x', 'new_cases_per_million.y')) %>%
-  rename(cdn_incidence =new_cases_per_million.x, us_incidence = new_cases_per_million.y) %>% 
-  pivot_longer(cols=!date) %>%
-  mutate(name=name %>% factor() %>% fct_relevel('us_incidence', 'cdn_incidence'))
+  rename(Canada =new_cases_per_million.x, US = new_cases_per_million.y) %>% 
+  pivot_longer(cols= !date) %>%
+  mutate(name=name %>% factor() %>% fct_relevel('US', 'Canada'))
 
-plot = ggplot(data_all, aes(x=date, y=value,color=name)) + geom_line()
+ggplot(data_all, aes(x=date, y=value,color=name)) + 
+  geom_line() +
+  labs(title='New cases per million', col='Country') +
+  scale_color_colorblind()
 
-plot = plot + labs(title='New cases per million in various countries')
-plot = plot + scale_color_colorblind()
-print(plot)
 ggsave('figures/real_dataviz.png', width=10.4, height=6.15)
